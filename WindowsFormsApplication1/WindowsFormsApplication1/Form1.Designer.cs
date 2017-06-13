e5a648ad0b589b5e2ef1ca3c6d8b57196965733d
Marco Rios <Marco Rios@DESKTOP-8QEN295>
2017-06-12 22:28:10 -0600

    progra botones primarios

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -43,7 +43,17 @@
             this.reproducirCancionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
@@ -51,48 +61,53 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(252, 273);
+            this.pictureBox4.Location = new System.Drawing.Point(187, 363);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(41, 34);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 23;
             this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(204, 272);
+            this.pictureBox3.Location = new System.Drawing.Point(106, 363);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(49, 35);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 22;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // pictureBox5
             // 
             this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(355, 273);
+            this.pictureBox5.Location = new System.Drawing.Point(358, 364);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(41, 34);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 24;
             this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(292, 260);
+            this.pictureBox2.Location = new System.Drawing.Point(268, 351);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(64, 57);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 21;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // listBox1
             // 
@@ -136,10 +151,11 @@
             this.misListasToolStripMenuItem,
             this.agregarCancionABibliotecaToolStripMenuItem,
             this.reproducirCancionToolStripMenuItem,
-            this.opcionesToolStripMenuItem});
+            this.opcionesToolStripMenuItem,
+            this.acercaDeToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(710, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(892, 24);
             this.menuStrip1.TabIndex = 18;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -172,18 +188,117 @@
             // salirToolStripMenuItem
             // 
             this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
             this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.autorToolStripMenuItem});
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de";
+            // 
+            // autorToolStripMenuItem
+            // 
+            this.autorToolStripMenuItem.Name = "autorToolStripMenuItem";
+            this.autorToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.autorToolStripMenuItem.Text = "Autor";
+            this.autorToolStripMenuItem.Click += new System.EventHandler(this.autorToolStripMenuItem_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(726, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(726, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "label1";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(705, 200);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 30;
+            this.button5.Text = "Siguiente";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(705, 148);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 29;
+            this.button4.Text = "Detener";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(707, 305);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 28;
+            this.button3.Text = "Play";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(707, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "Anterior";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(705, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 49);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Agregar Cancion a Biblioteca";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(84, 290);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(315, 55);
+            this.axWindowsMediaPlayer1.TabIndex = 33;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(710, 365);
+            this.ClientSize = new System.Drawing.Size(892, 452);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox5);
@@ -204,6 +319,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -226,6 +342,16 @@
         private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autorToolStripMenuItem;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
+using System.Data; 
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,7 +15,20 @@ using System.Xml.Linq;
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
+
     {
+        int contador = -1;
+
+        List<album> cancioness = new List<album>();
+
+        public string nom;
+        public List<string> lista = new List<string>();
+        public string can = "";
+
+
+        XmlDocument xDoc = new XmlDocument();
+        XmlDocument xDoc2 = new XmlDocument();
+
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +36,286 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            pictureBox1.Image = new System.Drawing.Bitmap(@"H:\reproductor\reproductor\bin\Debug\imagenes\biblioteca.jpg");
+            //I:\reproductor\reproductor\bin\Debug\imagenes }
+            button1.Visible = false;
 
+            //mostrar lista
+
+            XmlDocument xDoc = new XmlDocument();
+
+
+            xDoc.Load(@"H:\reproductor\reproductor\bin\Debug\musicas.txt");
+
+
+
+            XmlNodeList canciones = xDoc.GetElementsByTagName("canciones");
+            XmlNodeList lista = ((XmlElement)canciones[0]).GetElementsByTagName("cancion");
+
+
+            foreach (XmlElement nodo in lista)
+            {
+                int i = 0;
+
+
+                album musicatemp = new album();
+
+
+                XmlNodeList Nombre = nodo.GetElementsByTagName("nombre");
+                XmlNodeList Ubicacion = nodo.GetElementsByTagName("ubicacion");
+
+
+                musicatemp.Nombre = Nombre[i].InnerText;
+                musicatemp.Ubicacion = Ubicacion[i].InnerText;
+
+
+
+                cancioness.Add(musicatemp);
+                contador = contador + 1;
+            }
+
+
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+
+            dataGridView1.DataSource = cancioness;
+
+            dataGridView1.Columns[0].DisplayIndex = 1;
+
+            dataGridView1.Update();
+
+
+
+            //////
+
+        }
+
+
+    
+
+    private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            int ind;
+
+            ind = dataGridView1.CurrentRow.Index;
+
+            axWindowsMediaPlayer1.URL = dataGridView1[0, ind].Value.ToString();
+
+            label1.Text = Path.GetFileNameWithoutExtension(axWindowsMediaPlayer1.URL);
+
+            try
+            {
+                pictureBox1.Image = new System.Drawing.Bitmap(@"H:\reproductor\reproductor\bin\Debug\imagenes\" + label1.Text + ".jpg");
+                //I:\reproductor\reproductor\bin\Debug\imagenes }
+
+                listBox1.Items.Clear();
+                listBox1.Visible = true;
+
+                StreamReader R = new StreamReader(@"H:\reproductor\reproductor\bin\Debug\letras\" + label1.Text + ".txt");
+                string r;
+                while ((r = R.ReadLine()) != null)
+                {
+                    listBox1.Items.Add(r);
+                }
+                R.Close();
+            }
+            catch (Exception)
+            {
+                listBox1.Visible = false;
+                pictureBox1.Image = new System.Drawing.Bitmap(@"H:\reproductor\reproductor\bin\Debug\imagenes\biblioteca.jpg");
+                //I:\reproductor\reproductor\bin\Debug\imagenes }
+
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            int ind;
+            ind = dataGridView1.CurrentRow.Index;
+
+
+
+            //obtiene el valor de la celda
+            // int cont=dataGridView1.RowCount ;
+            // int asu=0;
+
+
+            if (ind > 0)
+            {
+
+
+                dataGridView1.CurrentCell = dataGridView1[0, ind - 1];
+
+
+                int inu;
+
+                inu = dataGridView1.CurrentRow.Index;
+
+                axWindowsMediaPlayer1.URL = dataGridView1[0, inu].Value.ToString();
+                label1.Text = Path.GetFileNameWithoutExtension(axWindowsMediaPlayer1.URL);
+
+                try
+                {
+                    pictureBox1.Image = new System.Drawing.Bitmap(@"H:\reproductor\reproductor\bin\Debug\imagenes\" + label1.Text + ".jpg");
+                    //I:\reproductor\reproductor\bin\Debug\imagenes }
+                    listBox1.Items.Clear();
+                    listBox1.Visible = true;
+                    StreamReader R = new StreamReader(@"H:\reproductor\reproductor\bin\Debug\letras\" + label1.Text + ".txt");
+                    string r;
+                    while ((r = R.ReadLine()) != null)
+                    {
+                        listBox1.Items.Add(r);
+                    }
+                    R.Close();
+                }
+                catch (Exception)
+                {
+                    listBox1.Visible = false;
+                    pictureBox1.Image = new System.Drawing.Bitmap(@"H:\reproductor\reproductor\bin\Debug\imagenes\biblioteca.jpg");
+                    //I:\reproductor\reproductor\bin\Debug\imagenes }
+                }
+
+
+            }
+            else
+            {
+
+                dataGridView1.CurrentCell = dataGridView1[0, contador];
+
+                int inu;
+
+                inu = dataGridView1.CurrentRow.Index;
+
+                axWindowsMediaPlayer1.URL = dataGridView1[0, inu].Value.ToString();
+                label1.Text = Path.GetFileNameWithoutExtension(axWindowsMediaPlayer1.URL);
+
+                try
+                {
+                    pictureBox1.Image = new System.Drawing.Bitmap(@"H:\reproductor\reproductor\bin\Debug\imagenes\" + label1.Text + ".jpg");
+                    //I:\reproductor\reproductor\bin\Debug\imagenes }
+                    listBox1.Items.Clear();
+
+                    listBox1.Visible = true;
+                    StreamReader R = new StreamReader(@"H:\reproductor\reproductor\bin\Debug\letras\" + label1.Text + ".txt");
+                    string r;
+                    while ((r = R.ReadLine()) != null)
+                    {
+                        listBox1.Items.Add(r);
+                    }
+                    R.Close();
+
+                }
+                catch (Exception)
+                {
+                    listBox1.Visible = false;
+                    pictureBox1.Image = new System.Drawing.Bitmap(@"H:\reproductor\reproductor\bin\Debug\imagenes\biblioteca.jpg");
+                    //I:\reproductor\reproductor\bin\Debug\imagenes }
+                }
+
+
+            }
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            int ind;
+            ind = dataGridView1.CurrentRow.Index;
+
+
+
+            //obtiene el valor de la celda
+            // int cont=dataGridView1.RowCount ;
+            // int asu=0;
+
+
+            if (ind < contador)
+            {
+
+
+                dataGridView1.CurrentCell = dataGridView1[0, ind + 1];
+
+
+
+                int inu;
+
+                inu = dataGridView1.CurrentRow.Index;
+
+                axWindowsMediaPlayer1.URL = dataGridView1[0, inu].Value.ToString();
+                label1.Text = Path.GetFileNameWithoutExtension(axWindowsMediaPlayer1.URL);
+
+                try
+                {
+                    pictureBox1.Image = new System.Drawing.Bitmap(@"H:\reproductor\reproductor\bin\Debug\imagenes\" + label1.Text + ".jpg");
+                    //I:\reproductor\reproductor\bin\Debug\imagenes }
+
+                    listBox1.Items.Clear();
+                    listBox1.Visible = true;
+
+                    StreamReader R = new StreamReader(@"H:\reproductor\reproductor\bin\Debug\letras\" + label1.Text + ".txt");
+                    string r;
+                    while ((r = R.ReadLine()) != null)
+                    {
+                        listBox1.Items.Add(r);
+                    }
+                    R.Close();
+                }
+                catch (Exception)
+                {
+                    listBox1.Visible = false;
+                    pictureBox1.Image = new System.Drawing.Bitmap(@"H:\reproductor\reproductor\bin\Debug\imagenes\biblioteca.jpg");
+                    //I:\reproductor\reproductor\bin\Debug\imagenes }
+                }
+
+
+            }
+            else
+            {
+                dataGridView1.CurrentCell = dataGridView1[0, 0];
+                int inu;
+
+                inu = dataGridView1.CurrentRow.Index;
+
+                axWindowsMediaPlayer1.URL = dataGridView1[0, inu].Value.ToString();
+                label1.Text = Path.GetFileNameWithoutExtension(axWindowsMediaPlayer1.URL);
+
+                try
+                {
+                    pictureBox1.Image = new System.Drawing.Bitmap(@"H:\reproductor\reproductor\bin\Debug\imagenes\" + label1.Text + ".jpg");
+                    //I:\reproductor\reproductor\bin\Debug\imagenes }
+
+                    listBox1.Items.Clear();
+                    listBox1.Visible = true;
+
+                    StreamReader R = new StreamReader(@"H:\reproductor\reproductor\bin\Debug\letras\" + label1.Text + ".txt");
+                    string r;
+                    while ((r = R.ReadLine()) != null)
+                    {
+                        listBox1.Items.Add(r);
+                    }
+                    R.Close();
+                }
+                catch (Exception)
+                {
+                    listBox1.Visible = false;
+                    pictureBox1.Image = new System.Drawing.Bitmap(@"H:\reproductor\reproductor\bin\Debug\imagenes\biblioteca.jpg");
+                    //I:\reproductor\reproductor\bin\Debug\imagenes }
+                }
+            }
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void autorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
